03bc81919a74d8ff4b5608eee5bb1ee260321065
saiemshahed <81529449+saiemshahed@users.noreply.github.com>
2021-09-29 17:53:02 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens:{
+        sm:'576px',
+        md:'768px',
+        lg:'992px',
+        xl:'1200px'
+
+      },
+      container:{
+        center:true,
+        padding:'1rem'
+      },
+      fontFamily:{
+      popins:"'Poppins', sans-serif",
+      roboto:"'Roboto', sans-serif"
+      },
+      colors:{
+        'primary': "#FD3D57"
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display:['group-hover'],
+      visibility:['group-hover']
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
